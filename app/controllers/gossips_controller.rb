5bbb72ed2e6b98858b3146bdffdbfa2b870e0fcf
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-02-16 20:58:08 +0100

    gossip for logged user

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -13,17 +13,22 @@ class GossipsController < ApplicationController
 
   def new
     @gossip = Gossip.new(
-      'title' => params[:title],
-      'content' => params[:content])
+      title: params[:title],
+      content: params[:content])
   end
 
   def create
-    @gossip = Gossip.new!(title: params[:title], content: params[:content], user_id: 5)
+    @user = current_user
+    @gossip = Gossip.new(title: params[:title], content: params[:content], user_id: @user.id)
+    puts "*" * 30
+    puts @user.id
+    puts "*" * 30
     
-    if @gossip.save # essaie de sauvegarder en base @gossip
+    if @gossip.save! # essaie de sauvegarder en base @gossip
       redirect_to welcome_path  # si ça marche, il redirige vers la page d'index du site
     else
-      render :new # sinon, il render la view new (qui est celle sur laquelle on est déjà)
+      render :new 
+      puts "failed!" # sinon, il render la view new (qui est celle sur laquelle on est déjà)
     end
 
   end
